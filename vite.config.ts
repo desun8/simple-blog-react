0cce708f71063440a8543ae728d2a629fa4bf7f6
Evgenii Pchelinov <epchelinov@gmail.com>
2025-09-01 22:27:32 +0500

    feat(config): setup path aliases and vite-tsconfig-paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
+import tsconfigPath from 'vite-tsconfig-paths';
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tsconfigPath(),
     tanstackRouter({
       routesDirectory: "./src/app/routes",
       generatedRouteTree: "./src/app/routes/routeTree.gen.ts",
